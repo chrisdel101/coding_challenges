098f11a9e9b183c9fcb0ef3b249d8a22e3fb769c
Chris Del <chris.dielschnieder@virtualhighschool.com>
2018-01-19 15:46:52 -0500

    add regex function to search, added to html maker

--- freeCodeCamp/twitch/script.js
+++ freeCodeCamp/twitch/script.js
@@ -1,207 +1,219 @@
 
 const users = ["freecodecamp","ESL_SC2", "OgamingSC2", "cretetion", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-var globalUserStreamData    = [];//online and username
-var globalUserChannelData   = [];//all other info
+const globalUserStreamData    = [];//online and username
+const globalUserChannelData   = [];//all other info
+const input = document.querySelector("#search-input")
 
 // make ajax call to find out if user is online- function should pass true or false
 //make ajax call with userdata to get logo, url stream, and if account exists
 
 makeAjax(`https://api.twitch.tv/kraken/streams/`, users,(res) => {
-    res.forEach((userObj) => {
-        var userData = {}
-        // console.log(userObj)
+  res.forEach((userObj) => {
+    var userData = {}
+    // console.log(userObj)
 
-        // get user name in url
-        let user = userObj._links.self
-        // seperate to get username alone
-        let userName = getUserName(user)
-        // push to blank object
-        userData['username'] = userName
+    // get user name in url
+    let user = userObj._links.self
+    // seperate to get username alone
+    let userName = getUserName(user)
+    // push to blank object
+    userData['username'] = userName
 
-        // check if useronline
-        if(userObj.stream != null){
-            userData['online'] = true;
-        } else {
-            userData['online'] = false
-        }
+    // check if useronline
+    if(userObj.stream != null){
+      userData['online'] = true;
+    } else {
+      userData['online'] = false
+    }
 
-        // get user live feed
-        // if(userObj.stream != null){
-        //     let feed = userObj.stream.channel.url
-        //     userData['feed'] = feed;
-        // } else {
-        //     userData['feed'] = "offline";
-        // }
-        // if(userObj.stream != null){
-        //     let game = userObj.stream.channel.game
-        //     userData['game'] = game;
-        // } else {
-        //     userData['feed'] = null;
-        // }
-        // if(userObj.stream != null){
-        //     let logo = userObj.stream.channel.logo
-        //     userData['logo'] = logo;
-        // } else  {
-        //     userData['logo'] = "offline"
-        // }
-        //
-        // console.log(userData)
-        // var html = makeHTML(userData)
-        // displayResults(html,"#results")
+    // get user live feed
+    // if(userObj.stream != null){
+    //     let feed = userObj.stream.channel.url
+    //     userData['feed'] = feed;
+    // } else {
+    //     userData['feed'] = "offline";
+    // }
+    // if(userObj.stream != null){
+    //     let game = userObj.stream.channel.game
+    //     userData['game'] = game;
+    // } else {
+    //     userData['feed'] = null;
+    // }
+    // if(userObj.stream != null){
+    //     let logo = userObj.stream.channel.logo
+    //     userData['logo'] = logo;
+    // } else  {
+    //     userData['logo'] = "offline"
+    // }
+    //
+    // console.log(userData)
+    // var html = fillHTMLtemplate(userData)
+    // displayResults(html,"#results")
 
-        globalUserStreamData.push(userData)
-    })
+    globalUserStreamData.push(userData)
+  })
 })
 
 makeAjax(`https://api.twitch.tv/kraken/channels/`, users,(res) => {
 
-        // callback does not return a value here
-        setOnlineStatus(res,() => {
-          // make html out of user data
-            let html = makeHTML(globalUserChannelData)
-            // // put html into display logic
-            displayResults(html, "#results")
-            // select all classes in nodelist
-            let feeds = document.querySelectorAll('.feed')
-            let resultsContainers = document.querySelectorAll('.results-container')
-            let onlineStatus = document.querySelectorAll('.online-status')
-            console.log(onlineStatus)
-            // loop through json
-            globalUserChannelData.forEach((user,index) => {
-                // if true, use index to set that one to addclass
-                if(user.online === true){
-                    onlineStatus[index].classList.add('online');
-                } else {
-                    onlineStatus[index].classList.add('offline');
-                }
-                if(user.online === true){
-                    resultsContainers[index].classList.add('online')
-                } else {
-                    resultsContainers[index].classList.add('offline')
+  // callback does not return a value here
+  setOnlineStatus(res,() => {
+    // make html out of user data
+    let html = fillHTMLtemplate(globalUserChannelData)
+    // // put html into display logic
+    displayResults(html, "#results")
+    // select all classes in nodelist
+    let feeds = document.querySelectorAll('.feed')
+    let resultsContainers = document.querySelectorAll('.results-container')
+    let onlineStatus = document.querySelectorAll('.online-status')
+    console.log(onlineStatus)
+    // loop through json
+    globalUserChannelData.forEach((user,index) => {
+      // if true, use index to set that one to addclass
+      if(user.online === true){
+        onlineStatus[index].classList.add('online');
+      } else {
+        onlineStatus[index].classList.add('offline');
+      }
+      if(user.online === true){
+        resultsContainers[index].classList.add('online')
+      } else {
+        resultsContainers[index].classList.add('offline')
 
-                }
+      }
 
-            })
-        })
+    })
+  })
 
 })
 
-
 // on call to /channel make userObj, and call array merge logic
 function setOnlineStatus(response,callback){
-    response.forEach((userObj) => {
-        // console.log(userObj)
-        let userData = {}
-        // check user is active
-        if(userObj.status != null){
-            let userName = userObj.display_name;
-            let status = userObj.status;
-            let feed = userObj.url;
-            let logo = userObj.logo;
-            userData = {
-                'username':userName,
-                'status':status,
-                'feed': feed,
-                'logo':logo
-            }
-        } else {
-            // stop if value is null
-            return
-        }
-        // push to global array
-        globalUserChannelData.push(userData)
-    })
-    // settimeout before calling running displayLogic
-    setTimeout(function(){
-        callback(displayLogic(globalUserChannelData))
-    },0010)
+  response.forEach((userObj) => {
+    // console.log(userObj)
+    let userData = {}
+    // check user is active
+    if(userObj.status != null){
+      let userName = userObj.display_name;
+      let status = userObj.status;
+      let feed = userObj.url;
+      let logo = userObj.logo;
+      userData = {
+        'username':userName,
+        'status':status,
+        'feed': feed,
+        'logo':logo
+      }
+    } else {
+      // stop if value is null
+      return
+    }
+    // push to global array
+    globalUserChannelData.push(userData)
+  })
+  // settimeout before calling running displayLogic
+  setTimeout(function(){
+    callback(displayLogic(globalUserChannelData))
+  },0010)
 }
 function makeAjax(url, arr, callback){
-    let results = [];
-    arr.forEach((i) => {
-        let myHeaders = new Headers();
-        myHeaders.append('Client-ID','g8oo57hf3026alyweepj1ov6rg6p5q')
-        let options = {
-            method: 'GET',
-            headers: myHeaders,
-        }
-        fetch(`${url}/${i}`,options)
-        .then(blob => blob.json ())
-        .then((data) => { results.push(data)})
+  let results = [];
+  arr.forEach((i) => {
+    let myHeaders = new Headers();
+    myHeaders.append('Client-ID','g8oo57hf3026alyweepj1ov6rg6p5q')
+    let options = {
+      method: 'GET',
+      headers: myHeaders,
+    }
+    fetch(`${url}/${i}`,options)
+    .then(blob => blob.json ())
+    .then((data) => { results.push(data)})
 
-    })
-    // returns an array
-    setTimeout(function(){
-        callback(results)
-    },1000)
+  })
+  // returns an array
+  setTimeout(function(){
+    callback(results)
+  },1000)
 }
 
 
 
 function getUserName(str){
-    str = str.match(/([^/]*)$/)[1]
-    return str
+  str = str.match(/([^/]*)$/)[1]
+  return str
 }
 
 // checks each array for user and adds online status- leaving one main array
 function displayLogic(){
-    // loop streams to check if online
-    globalUserStreamData.forEach((stream) => {
-        // console.log(stream.online)
-        if(stream.online === true){
-        // if online, add attr to channel data
-            var sameUser = globalUserChannelData.find((val) => {
-                // make toLowerCase, check for undefined
-                if(val != undefined){
-                    return val.username.toLowerCase() == stream.username.toLowerCase()
-                }
-            })
-            if(sameUser != undefined){
-                sameUser['online'] = true;
-            }
-        } else {
-        // if offline, add attr to channel data
-            var sameUser = globalUserChannelData.find((val) => {
-                if(val != undefined){
-                    return val.username.toLowerCase() === stream.username.toLowerCase()
-                }
-            })
-            if(sameUser != undefined){
-                sameUser['online'] = false;
-            }
+  // loop streams to check if online
+  globalUserStreamData.forEach((stream) => {
+    // console.log(stream.online)
+    if(stream.online === true){
+      // if online, add attr to channel data
+      var sameUser = globalUserChannelData.find((val) => {
+        // make toLowerCase, check for undefined
+        if(val != undefined){
+          return val.username.toLowerCase() == stream.username.toLowerCase()
         }
-    })
+      })
+      if(sameUser != undefined){
+        sameUser['online'] = true;
+      }
+    } else {
+      // if offline, add attr to channel data
+      var sameUser = globalUserChannelData.find((val) => {
+        if(val != undefined){
+          return val.username.toLowerCase() === stream.username.toLowerCase()
+        }
+      })
+      if(sameUser != undefined){
+        sameUser['online'] = false;
+
+      }
+    }
+  })
 
 }
 
-
+function  findMatches(wordToMatch,users){
+  return users.filter(user => {
+    // make regex with word, global, case insenstive
+    const regex = new RegExp(wordToMatch, 'gi')
+    return user.username.match(regex) || user.feed.match(regex)
+  })
+}
 // takes block of html and appends to css id
 function displayResults(html,domNode){
-    // make id a selectable node
-    var domNode = document.querySelector(domNode);
-    // select all containers
-    var div = document.querySelectorAll('.results-container');
-    //
-    // add class to all divs in block
-    if(div != null){
-        // since first is null
-        div.forEach(i => i.classList.add('show'))
-    }
-    // insertAdjacentHTML new api works - innerHTML does not- before end is part of api
-    domNode.insertAdjacentHTML('beforeend',html);
+  // make id a selectable node
+  var domNode = document.querySelector(domNode);
+
+  // insertAdjacentHTML new api works - innerHTML does not- before end is part of api
+  domNode.insertAdjacentHTML('beforeend',html);
+
 }
 function makeHTML(arr){
-    var markup = arr.map((user) => {
-        // console.log(user.username)
-        return `
-        <div class="results-container hidden">
-            <li class="list-item title feed"><a href=${user.feed}>${user.username}</a></li>
-            <li class="list-item online-status"></li>
-            <li class="list-item"><img class="logo" src=${user.logo}></li>
-            </div>
-            `
-    }).join('')
-    return markup
+  // if input is empty, display all
+  if(input.value === ""){
+    fillHTMLtemplate(arr)
+  } else {
+    let matches = findMatches(arr,input.value)
+    fillHTMLtemplate(matches)
+  }
+
+}
+
+function fillHTMLtemplate(arr){
+  var markup = arr.map((user) => {
+    // console.log(user.username)
+    return `
+    <div class="results-container">
+    <li class="list-item title feed"><a href=${user.feed}>${user.username}</a></li>
+    <li class="list-item online-status"></li>
+    <li class="list-item"><img class="logo" src=${user.logo}></li>
+    </div>
+    `
+  }).join('')
+  return markup
 }
 
 
@@ -236,9 +248,9 @@ function makeHTML(arr){
 //             console.log(data)
 //         })
 //     })
-    // setTimeout(function(){
-    //     callback(results)
-    // },1000)
+// setTimeout(function(){
+//     callback(results)
+// },1000)
 
 // makeAjax('https://api.twitch.tv/kraken/channels/twitch',(res) => {
 //     console.log(res)
