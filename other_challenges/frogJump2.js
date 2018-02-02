5322c029229eee7db56708d03d58248ac86a1d3f
Chris Del <chris.dielschnieder@virtualhighschool.com>
2018-02-02 15:58:30 -0500

    added for loop frog solution

--- other_challenges/frogJump2.js
+++ other_challenges/frogJump2.js
@@ -1,14 +1,31 @@
 // to get the counter to work, it must be passed in as an argument
 
-function solution(start,end,jumpLength, count = 0){
-	if(start >= end){
-		return
-    }
-    if(start < end){
-        start = start + jumpLength;
-		solution(start,end,jumpLength, count + 1)
-    }
-
-}
-
-solution(0,100,10)
+// call stack exceeds at 99999
+// function solution(start,end,jumpLength, count = 0){
+//     if((start > end) || (jumpLength === 0)){
+// 		return count;
+//     }
+//     if(start < end){
+//         start = start + jumpLength;
+//  				// console.log(count)
+// 		return solution(start,end,jumpLength, count + 1)
+//
+//     }
+// 	return count
+// }
+// console.log(solution(0,99991,10,0))
+function solution(start,end,jumpLength){
+     var count = 0;
+     // if((jumpLength <= 0) || (start < end)){
+     //     throw(err)
+     //     return;
+     // }
+     var fullLength = end - start;
+     var i;
+     for (i = 0; i < fullLength; i += jumpLength) {
+         // console.log(i)
+         count++
+     }
+     return count
+ }
+solution(0,100,10,0)
