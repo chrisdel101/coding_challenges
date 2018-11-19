89e3e5f038bf80aa5f2c7c8fa1c62256311edce9
Chris Del <drumgod101@gmail.com>
2018-11-18 22:47:46 -0500

    complete imessy iteration that passes correctness

--- other_challenges/triangle2.js
+++ other_challenges/triangle2.js
@@ -1,55 +1,15 @@
-// A[P] + A[Q] > A[R],
-// A[Q] + A[R] > A[P],
-// A[R] + A[P] > A[Q]
-//
-// p= 10
-// q= 5
-// r= 8
-// 10 + 5 > 8
-// 5 + 8 > 10
-// 8 + 10 > 5
-//
-// p= 5
-// q= 10
-// r= 8
-// 5 + 10 > 8
-// 10 + 8 > 5
-// 8+ 5 > 10
-//
-//
-
-10, 2, 5
-10, 2, 1
-10, 2, 8,
-10, 2, 20
-//
-// 10, 5, 1
-// 10, 5, 8
-// 10, 5, 20
-//
-// 10, 1, 8
-// 10, 1, 20
-//
-// 10, 8,`` 20
-let tuples = []
-// for (var i = 0; i < ; i++) {
-// 	let arrCopy = arr.slice()
-// 	arr.
-// }
-let arr = [10, 2, 5, 1, 8, 20]
-
 function solution(arr) {
+	let tuples = []
 	const originalArrLen = arr.length
 	let i = 0
 	let j = 0
-	let k = 0
 	// let start = arr[0]
 	// make arrCopy
 	let counter = 0
 	while (counter !== originalArrLen) {
-		console.log('oringal len', originalArrLen)
+		// 		console.log('oringal len', originalArrLen)
 		let arrOuterCopy = arr.slice()
-		console.log('outer', arrOuterCopy)
+		// 		console.log('outer', arrOuterCopy)
 		while (arrOuterCopy.length > 1) {
 			// make copy
 			let arrCopy = arrOuterCopy.slice()
@@ -82,42 +42,22 @@ function solution(arr) {
 			// arr.splice(0, 1)
 			// after runing SECOND rip, splice of first num
 			arrOuterCopy.splice(1, 1)
-			console.log('outer arr len', arrOuterCopy.length)
+			// 			console.log('outer arr len', arrOuterCopy.length)
 			// console.log(tuples)
 		}
 		counter++
 		arr.splice(0, 1)
-		console.log('arr', originalArrLen)
-		console.log('counter', counter)
+		// 		console.log('arr', originalArrLen)
+		// 		console.log('counter', counter)
 
 	}
-	return tuples
-}
-// solution(arr)
-// console.log(tuples)
-// A[P] + A[Q] > A[R],
-// A[Q] + A[R] > A[P],
-// A[R] + A[P] > A[Q]
-function checker(tuples) {
 	for (var k = 0; k < tuples.length; k++) {
+		console.log(tuples[k])
 		if ((tuples[k][0] + tuples[k][1] > tuples[k][2]) && (tuples[k][1] + tuples[k][2] > tuples[k][0]) && (tuples[k][2] + tuples[k][0] > tuples[k][1])) {
-			console.log('hello')
+			return 1
 		}
 	}
+
+	return 0
 }
-checker(solution(arr))
-// var start = arr[0]
-// while(arr.length){
-// 	for(var i=0;i<3;i++){
-
-// 	console.log(arr[i])
-// }
-// 	arr.splice(1,1)
-
-
-// }
-//
-// - set const to furthest left num
-// - while const, keep outer look going
-// - loop three times then cut the num next to const
-// - when at end quit
+console.log(solution([10, 2, 5, 1, 8, 20]))
