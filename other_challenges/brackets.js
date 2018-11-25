b48062be987be48e07a224a84c3ee53978a159c6
Chris Del <drumgod101@gmail.com>
2018-11-24 20:48:12 -0500

    complete brackets

--- other_challenges/brackets.js
+++ other_challenges/brackets.js
@@ -29,112 +29,112 @@
 // 	}
 // 	console.log('obj', obj)
 // 	return counter
+// // }
+// function solution(S) {
+// 	let bracketNums = []
+// 	for (var i = 0; i < S.length; i++) {
+// 		let current = S[i]
+//
+// 		switch (current) {
+// 			case "(":
+// 				bracketNums.push(1)
+// 				break
+// 			case ")":
+// 				bracketNums.push(2)
+// 				break
+// 			case "[":
+// 				bracketNums.push(3)
+// 				break
+// 			case "]":
+// 				bracketNums.push(4)
+// 				break
+// 			case "{":
+// 				bracketNums.push(5)
+// 				break
+// 			case "}":
+// 				bracketNums.push(6)
+// 				break
+// 			default:
+// 				console.log('no switch data')
+// 		}
+//
+// 	}
+// for (var i = 0; i < bracketNums.length; i++) {
+// 	if (bracketNums[i] === 2 || bracketNums[i] === 4 || bracketNums[i] === 6) {
+// 		// let previous = previousNum(bracketNums[i])
+// 		if (bracketNums.includes(previous)) {
+// 		// 	let currentIndex = bracketNums.indexOf(bracketNums[i])
+// 		// 	let previousIndex = bracketNums.indexOf(previous)
+// 		// 	console.log('CI', currentIndex)
+// 		// 	console.log('PI', previousIndex)
+// 		}
+//
+// // 	}
+// // }
+// var countedNums = bracketNums.reduce(function(allNums, num) {
+// 	if (num in allNums) {
+// 		allNums[num]++;
+// 	} else {
+// 		allNums[num] = 1;
+// 	}
+//
+// 	return allNums;
+// }, {});
+// console.log(countedNums)
+// for (var num in countedNums) {
+// 	//
+// 		if (num === '2' || num === '4' || num === '6') {
+// 			let previous = previousNum(num)
+// 			console.log('key', previous)
+// 			console.log('val', countedNums[previous])
+// 			// count of 1, 3 and 5
+// 			let previousAmount = countedNums[previous]
+// 			let currentAmount = countedNums[num]
+// 			// console.log('CA', currentAmount)
+// 			console.log('key', num)
+// 			console.log('val', currentAmount)
+// 			// console.log(previousAmount)
+// 			// if previous undefined
+// 			if (!previousAmount) {
+// 				console.log('UNDEFINED')
+// 				return 0
+// 				// } //else if (previousAmount < ){
+//
+// 				// }
+//
+// 			}
+// 		}
+// 		// console.log(countedNums)
+//
+// 	}
 // }
-function solution(S) {
-	let bracketNums = []
-	for (var i = 0; i < S.length; i++) {
-		let current = S[i]
-
-		switch (current) {
-			case "(":
-				bracketNums.push(1)
-				break
-			case ")":
-				bracketNums.push(2)
-				break
-			case "[":
-				bracketNums.push(3)
-				break
-			case "]":
-				bracketNums.push(4)
-				break
-			case "{":
-				bracketNums.push(5)
-				break
-			case "}":
-				bracketNums.push(6)
-				break
-			default:
-				console.log('no switch data')
-		}
-
-	}
-	// for (var i = 0; i < bracketNums.length; i++) {
-	// 	if (bracketNums[i] === 2 || bracketNums[i] === 4 || bracketNums[i] === 6) {
-	// 		// let previous = previousNum(bracketNums[i])
-	// 		if (bracketNums.includes(previous)) {
-	// 		// 	let currentIndex = bracketNums.indexOf(bracketNums[i])
-	// 		// 	let previousIndex = bracketNums.indexOf(previous)
-	// 		// 	console.log('CI', currentIndex)
-	// 		// 	console.log('PI', previousIndex)
-	// 		}
-	//
-	// 	}
-	// }
-	var countedNums = bracketNums.reduce(function(allNums, num) {
-		if (num in allNums) {
-			allNums[num]++;
-		} else {
-			allNums[num] = 1;
-		}
-
-		return allNums;
-	}, {});
-	console.log(countedNums)
-	for (var num in countedNums) {
-
-		if (num === '2' || num === '4' || num === '6') {
-			let previous = previousNum(num)
-			console.log('key', previous)
-			console.log('val', countedNums[previous])
-			// count of 1, 3 and 5
-			let previousAmount = countedNums[previous]
-			let currentAmount = countedNums[num]
-			// console.log('CA', currentAmount)
-			console.log('key', num)
-			console.log('val', currentAmount)
-			// console.log(previousAmount)
-			// if previous undefined
-			if (!previousAmount) {
-				console.log('UNDEFINED')
-				return 0
-				// } //else if (previousAmount < ){
-
-				// }
-
-			}
-		}
-		// console.log(countedNums)
-
-	}
-}
 
 function previousNum(num) {
 	return num - 1
 
 }
-// function oppositeBracket(bracket) {
-// 	let opposite
-// 	if (bracket === ")") {
-// 		opposite = "("
-// 	} else if (bracket === "(") {
-// 		opposite = ")"
-// 	} else if (bracket === "[") {
-// 		opposite = "]"
-// 	} else if (bracket === "]") {
-// 		opposite = "["
-// 	} else if (bracket === "}") {
-// 		opposite = "{"
-// 	} else if (bracket === "{") {
-// 		opposite = "}"
-// 	} else {
-// 		console.log('no bracket value')
-// 	}
-// 	return opposite
-// }
+
+function oppositeBracket(bracket) {
+	let opposite
+	if (bracket === ")") {
+		opposite = "("
+	} else if (bracket === "(") {
+		opposite = ")"
+	} else if (bracket === "[") {
+		opposite = "]"
+	} else if (bracket === "]") {
+		opposite = "["
+	} else if (bracket === "}") {
+		opposite = "{"
+	} else if (bracket === "{") {
+		opposite = "}"
+	} else {
+		console.log('no bracket value')
+	}
+	return opposite
+}
 // [()()
 // solution("{[()()]}")
-console.log(solution("()){}]](())"))
 
 // "(".charCodeAt()
 // 40
@@ -168,3 +168,75 @@ console.log(solution("()){}]](())"))
 // )
 // }
 // }
+// STACK attempt
+// function solution(S) {
+// 	let stack = []
+// 	S = S.split("")
+// 	for (var i = 0; i < S.length; i++) {
+// 		let current = S[i]
+// 		console.log('current', current)
+// 		// if opening push onto stack
+// 		if (current === "(" || current === "[" || current === "{") {
+// 			console.log('push', current)
+// 			stack.push(current)
+// 			// if closing check first for match
+// 		} else {
+// 			// this is top of stack
+// 			let last = stack.pop() //pop last off
+// 			console.log('last', last, )
+//
+// 			// opposite of current closing
+// 			let openingBracket = oppositeBracket(current)
+// 			// console.log('openingBracket', openingBracket)
+// 			// console.log('last', last)
+// 			// if undefined then no match
+// 			console.log('check if last matches opposite of current')
+// 			if (!last || !last === openingBracket) {
+// 				console.log('no match')
+// 				return 0
+// 			}
+// 		}
+// 	}
+// 	console.log(stack)
+// 	return 1
+// }
+// CORRECT SOLUTION
+function solution(S) {
+	let arr = []
+	S = S.split("")
+	for (var i = 0; i < S.length; i++) {
+		let current = S[i]
+		// console.log('current', current)
+		// if opening push onto stack
+		if (current === "(" || current === "[" || current === "{") {
+			// console.log('puish', current)
+			arr.push(current)
+			// if closing check first for match
+		} else {
+			// opposite of current closing
+			let openingBracket = oppositeBracket(current)
+			// check last in opening for matches
+			if (arr[arr.length - 1] === openingBracket) {
+				// if matches, slice off the last one
+				arr.pop()
+			} else {
+				// if not, then nested wrong
+				return 0
+			}
+			//
+
+
+		}
+	}
+	// check if all opening brackets popped
+	if (arr.length > 0) {
+		// in case all opening brackets only
+		return 0
+	}
+	// console.log(arr)
+	return 1
+}
+
+// console.log(solution('([)()]'))
+// console.log(solution("([])"))
+console.log(solution("{{{"))
