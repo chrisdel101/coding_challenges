e561289f0cfb91f127ec69e04c868ad6e9be8cba
Chris Del <drumgod101@gmail.com>
2018-12-01 22:58:36 -0500

    complete supermarket queue. Adding comments

--- code_wars/js/supermarket2.js
+++ code_wars/js/supermarket2.js
@@ -1,5 +1,5 @@
 function queueTime(customers, n) {
-	// each customer has a range for the minutes
+	// each customer has a range - make array of arrays of ranges
 	let customerRanges = []
 	let counter = 0
 	for (var j = 0; j < customers.length; j++) {
@@ -14,59 +14,70 @@ function queueTime(customers, n) {
 		// reset arr store
 		tempArr = []
 	}
-	console.log('customer ranges', customerRanges)
+	// console.log('customer ranges', customerRanges)
 	let currentCustomer
 	let done = false
 	let i = 0
 	if (n > customerRanges.length) {
 		n = customerRanges.length
 	}
+	// loop over ranges
 	while (!done) {
 		let obj = {}
-		// keeps looping in twos
+		// loop n times
 		for (var k = 0; k < n; k++) {
 			// keep a pattern repeating over and over
+			// arr[0][1]
+			// arr[1][1]
+			//arr[2][1]/// Indexes stay the same and get reassigned
 			currentCustomer = customerRanges[k][i]
-			// if not undefined
+			// if undefined, concat next section to current
 			if (!currentCustomer) {
-				// k + n is next range to process
-				let left = customerRanges[k + n]
-				// if some left, conact it to the current chunk
+				let left = customerRanges[n]
+				// if some chunks left continue
 				if (left) {
-					// console.log('reassign')
-					// k + n concat to the chunk of customerRanges currently on
-					customerRanges[k] = customerRanges[k].concat(customerRanges[k + n])
-					console.log('arr', k + 1)
-					console.log('currentCus array', customerRanges[k])
+					// add next range from end to current index
+					customerRanges[k] = customerRanges[k].concat(customerRanges[n])
+					// console.log('arr', k + 1)
+					// console.log('index', n)
+					// console.log('chunk', customerRanges[n])
+					// console.log('currentCus array', customerRanges[k])
 					// console.log('splice', customerRanges[k + n])
 					// splice off the chunk added
-					customerRanges.splice(k + n, 1)
-					console.log('currentCus', customerRanges[k][i])
+					// console.log('spliced', customerRanges[n])
+					// splice off the concated part to shave arr length
+					customerRanges.splice(n, 1)
+					// console.log('whole', customerRanges)
+					// console.log('currentCus', customerRanges[k][i])
 				} else {
+					// if current line has not more to get from end, make it undefined
 					obj[k] = undefined
-					console.log('arr', k + 1)
-					console.log('done', customerRanges[k + n])
+					// console.log('arr', k + 1)
+					// console.log('done', customerRanges[k + n])
 				}
-			} else {
-				// loop through
-				console.log('arr', k + 1)
-				console.log('currentCus', currentCustomer)
 			}
+			// else {
+			// 	// loop through
+			// 	// console.log('arr', k + 1)
+			// 	// console.log('currentCus', currentCustomer)
+			// 	// console.log(customerRanges[k])
+			// }
 		}
+		// base case to stop outer while loop
 		if ((Object.entries(obj).length >= n) && (Object.values(obj).every(i => i === undefined))) {
-			console.log('true')
+			// console.log('true')
 			done = true
 			break
 		}
-		console.log('obj', obj)
-		console.log('i', i)
+		// console.log('obj', obj)
+		// console.log('i', i)
 		i++
 	}
-	// console.log('i++', i)
-	// console.log('CR', customerRanges)
+
 	return i
 }
-console.log(queueTime([1, 2, 3, 4, 5], 100))
+console.log(queueTime([1, 2, 3, 4], 1))
+// console.log(queueTime([2, 2, 3, 3, 4, 4], 2))
 //
 // [7,10,11,14],2
 //
