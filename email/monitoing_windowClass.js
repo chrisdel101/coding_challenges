a2682edee28afcfccf2b7b607a8bb891007e01f7
Chris Del <drumgod101@gmail.com>
2018-12-16 16:32:21 -0500

    complete challenge. Passing all tests

--- email/monitoing_windowClass.js
+++ email/monitoing_windowClass.js
@@ -1,278 +1,228 @@
 class MonitoringWindow {
-	constructor(list){
+	constructor(list) {
 		this.vistorsPerMin = list
 	}
-    // UTILITY FUNC - check input for errors
-    //takes an array - insert at first input point
-    //returns errors if truthy
-    _errorChecker(arr){
-        // check for all null
-        let allNull = arr.every(item => {
-            return item === null
-        })
-        if(allNull){
-            throw TypeError("All vals are null: cannot run function on null values")
-        }
-        if(arr.length <= 0){
-            throw TypeError("Empty array: cannot run function on empty array.")
-        }
 
-    }
-    // UTILITY FUNC - get all sub arrays between the nulls
-    //takes an array of nums and nulls
-    //returns arr of tuples - all vals in  between nulls
-    _makeIntervals(arr){
+	// UTILITY FUNC - check input for errors
+	//takes an array - insert at first input point
+	//returns errors if truthy
+	_errorChecker(arr) {
+		// check for all null
+		let allNull = arr.every(item => {
+			return item === null
+		})
+		if(allNull) {
+			throw TypeError("All vals are null: cannot run function on null values")
+		}
+		if(arr.length <= 0) {
+			throw TypeError("Empty array: cannot run function on empty array.")
+		}
+	}
 
-        this._errorChecker(arr)
-        let windows = []
-        let nullIndex = 0
-        let windowStartIndex = 0
-        for (var i = 0; i < arr.length; i++) {
-            if(arr[i] === null){
-                // set current nullIndex - end of slice path
-                nullIndex = i
-                // slice from start to nullIndex
-                let window = arr.slice(windowStartIndex, nullIndex)
-                // save window to arr
-                windows.push(window)
-                // if next is not null, set windowStartIndex to next item
-                if(arr[i + 1] !== null){
-                    // sets after the last null in arr - for get the nums after it
-                    windowStartIndex = i + 1
-                }
-            }
-        }
-        // if last val not null, get last window here
-        if(nullIndex !== arr.length - 1){
-            let finalWindow = arr.slice(windowStartIndex, (arr.length - 1) + 1 )
-            windows.push(finalWindow)
-        }
-        return windows
-    }
-    // UTILITY FUNC - creates every instance of possible monitoring windows
-    //takes array of tuples with btw null values
-    //returns arr of tuples - all possible window combos
-    _getMonitorWindows(arrOfTuples) {
-        let tuples = []
-        let storageArr = []
-        let j = 0
-        let copy
-        arrOfTuples.forEach(arr => {
-            // loop over each item in arr - reset counter to zero at each
-             while(arr.length) {
-                 let current = arr[j]
-                 // use BREAK to end loop
-                 while(true){
-                     storageArr.push(arr[j])
-                     // stop when undefined
-                     if(!arr[j]){
-                             // slice off first digit - decrease arr by one
-                             arr.splice(0,1)
-                             // reset inner counter
-                             j = 0
-                             // reset storageArr to empty
-                             storageArr = []
-                             break
-                         }
-                     // copy storageArr
-                     copy = storageArr.slice()
-                     // push new item into copy
-                     tuples.push(copy)
-                     j++
-                }
-            }
-        })
-        return tuples
-    }
+	// UTILITY FUNC - get all sub arrays between the nulls
+	//takes an array of nums and nulls
+	//returns arr of tuples - all vals in  between nulls
+	_makeIntervals(arr) {
 
-    // UTILITY FUNC - finds the interesting monitor windows
-    // takes an array of tuples of all monitor windows
-    //returns array of strings represing the IMWs
-    _getIMW(arrOfTuples){
-        var IMWs = []
-        for(var i = 0; i < arrOfTuples.length; i++){
-        	let outerVal = arrOfTuples[i].toString()
-            // j is reset on each while start
-        	let j = 0
-        	while(j < arrOfTuples.length){
-        	       let innerVal = arrOfTuples[j].toString()
-        // 		if matches && index is not the same as outer val, break
-        	//make so not checking same val against itself
-        		let innerIndex = arrOfTuples.indexOf(arrOfTuples[j])
-        		if(innerVal.match(outerVal) && innerIndex !== i){
-        			break
-        // if made to end without matching, push to arr
-                } else if(j === arrOfTuples.length - 1){
-        			IMWs.push(outerVal)
-        		}
-        		j++
-        	}
-        }
-        return IMWs
-    }
-    // UTILITY FUNC - converts IMW strings into numsbers
-    // take array of interesting monitor windows strs
-    // returns array indentical in nums
-    _convertToInts(arr){
-        var numStrs = []
-        arr.forEach(block => {
+		this._errorChecker(arr)
+		let windows = []
+		let nullIndex = 0
+		let windowStartIndex = 0
+		for(var i = 0; i < arr.length; i++) {
+			if(arr[i] === null) {
+				// set current nullIndex - end of slice path
+				nullIndex = i
+				// slice from start to nullIndex
+				let window = arr.slice(windowStartIndex, nullIndex)
+				// save window to arr
+				windows.push(window)
+				// if next is not null, set windowStartIndex to next item
+				if(arr[i + 1] !== null) {
+					// sets after the last null in arr - for get the nums after it
+					windowStartIndex = i + 1
+				}
+			}
+		}
+		// if last val not null, get last window here
+		if(nullIndex !== arr.length - 1) {
+			let finalWindow = arr.slice(windowStartIndex, (arr.length - 1) + 1)
+			windows.push(finalWindow)
+		}
+		return windows
+	}
 
-        	let cursor = 0
-        	let tempStorage = []
-        	for(var i = 0; i < block.length; i++){
+	// UTILITY FUNC - creates every instance of possible monitoring windows
+	//takes array of tuples with btw null values
+	//returns arr of tuples - all possible window combos
+	_getMonitorWindows(arrOfTuples) {
+		let tuples = []
+		let storageArr = []
+		let j = 0
+		let copy
+		arrOfTuples.forEach(arr => {
+			// loop over each item in arr - reset counter to zero at each
+			while(arr.length) {
+				let current = arr[j]
+				// use BREAK to end loop
+				while(true) {
+					storageArr.push(arr[j])
+					// stop when undefined
+					if(!arr[j]) {
+						// slice off first digit - decrease arr by one
+						arr.splice(0, 1)
+						// reset inner counter
+						j = 0
+						// reset storageArr to empty
+						storageArr = []
+						break
+					}
+					// copy storageArr
+					copy = storageArr.slice()
+					// push new item into copy
+					tuples.push(copy)
+					j++
+				}
+			}
+		})
+		return tuples
+	}
 
-        		let current = block[i]
-        	// console.log('block', block)
-        	//check if block has commas- none, push whole num
-        	if(!block.includes(",")){
-        		// console.log('no commas. Val as is')
-        // 		block = parseInt(block)
-        		tempStorage.push(block)
-        		break
-        		// console.log('i', i)
-        	} else if(block.includes(",")) {
-        	if(current === ","){
-        // 	if commas, get all blocks btw commas
-        			let slice = block.slice(cursor, i)
-        		// console.log('i', i)
-        		// console.log('val here', current)
-        		// console.log(`slice from ${cursor} to ${i}`)
-        		// console.log('slice LEN', slice.length)
-        			// console.log('original cursor', cursor)
-        			cursor = i + 1
-        			// console.log('reset cursor', cursor)
-        		tempStorage.push(slice)
-        		//if at end of block
-        		} else if(i === block.length -1){
-        			let slice = block.slice(cursor, i+1)
-        	// console.log("END slice", slice)
-        		tempStorage.push(slice)
-        		}
-        	}
+	// UTILITY FUNC - finds the interesting monitor windows
+	// takes an array of tuples of all monitor windows
+	//returns array of strings represing the IMWs
+	_getIMW(arrOfTuples) {
+		var IMWs = []
+		for(var i = 0; i < arrOfTuples.length; i++) {
+			let outerVal = arrOfTuples[i].toString()
+			// j is reset on each while start
+			let j = 0
+			while(j < arrOfTuples.length) {
+				let innerVal = arrOfTuples[j].toString()
+				// if matches && index is not the same as outer val, break
+				//not checking against itself
+				let innerIndex = arrOfTuples.indexOf(arrOfTuples[j])
+				if(innerVal.match(outerVal) && innerIndex !== i) {
+					break
+					// if made to end without matching, push to arr
+				} else if(j === arrOfTuples.length - 1) {
+					IMWs.push(outerVal)
+				}
+				j++
+			}
+		}
+		return IMWs
+	}
 
-            }
-        	numStrs.push(tempStorage)
-        })
-        let nums = numStrs.map(arr => {
-            return arr.map(str => {
-                return parseInt(str)
-            })
-        })
-        return nums
-    }
+	// UTILITY FUNC - converts IMW strings into numsbers
+	// take array of interesting monitor windows strs
+	// returns array of tuples indentical to strs
+	_convertToInts(arr) {
+		var numStrs = []
+		arr.forEach(block => {
+			// track start of each slice point
+			let cursor = 0
+			let tempStorage = []
+			for(var i = 0; i < block.length; i++) {
+				let current = block[i]
+				//check if block has commas- none, push whole num
+				if(!block.includes(",")) {
+					tempStorage.push(block)
+					break
+					// if there are commas, use slice around commas
+				} else if(block.includes(",")) {
+					// if current is comma, slice from cursor to comma
+					if(current === ",") {
+						let slice = block.slice(cursor, i)
+						//move cursor to stop after cursor for next slice
+						cursor = i + 1
+						tempStorage.push(slice)
+						//if at end of block it's the last, so from cursor to end
+					} else if(i === block.length - 1) {
+						let slice = block.slice(cursor, i + 1)
+						tempStorage.push(slice)
+					}
+				}
 
-    // INSTANCE FUNC- call on instance to get IMW with the highest sum
-    //takes array of interesting monitor windows as strings
-    //returns the IMW with highest sum
-    maxSum(){
-        let intervals = this._makeIntervals(this.vistorsPerMin)
-        let getWindows = this._getMonitorWindows(intervals)
-        let getIMWs = this._getIMW(getWindows)
+			}
+			// push arr into outer arr
+			numStrs.push(tempStorage)
+		})
+		let nums = numStrs.map(arr => {
+			return arr.map(str => {
+				return parseInt(str)
+			})
+		})
+		return nums
+	}
 
-    // 	will be strings, make nums
-    	getIMWs = this._convertToInts(getIMWs)
-        console.log('wins', getIMWs)
-        //loop over each- return arr of window sums
-        let sums = getIMWs.map(arr => {
-            // console.log(arr)
-            return arr.reduce((accumulator, currentValue) => {
-                return accumulator + currentValue
-            })
-        })
-        // console.log(sums)
-        //find highest sum of window
-        let highSum = sums.reduce((a, b) => {
-            return Math.max(a, b)
-        })
-        // check which index this in the array of sums
-        let index = sums.indexOf(highSum)
-        // index into the original arr to get solution
-        let highestWindow = getIMWs[index]
-        return highestWindow
+	// INSTANCE FUNC- call on instance to get IMW with the highest sum
+	//takes array of interesting monitor windows as strings
+	//returns the IMW with highest sum
+	maxSum() {
+		let intervals = this._makeIntervals(this.vistorsPerMin)
+		let getWindows = this._getMonitorWindows(intervals)
+		let getIMWs = this._getIMW(getWindows)
+		// 	will be strings, make nums
+		getIMWs = this._convertToInts(getIMWs)
+		//loop over each- return arr of window sums
+		let sums = getIMWs.map(arr => {
+			return arr.reduce((accumulator, currentValue) => {
+				return accumulator + currentValue
+			})
+		})
+		//find highest sum of window
+		let highSum = sums.reduce((a, b) => {
+			return Math.max(a, b)
+		})
+		// check which index this in the array of sums
+		let index = sums.indexOf(highSum)
+		// index into the original arr to get solution
+		let highestWindow = getIMWs[index]
+		return highestWindow
 
-    }
+	}
 
-    // INSTANCE FUNC- call on instance to get IMW with the lowest average window
-    //takes array of interesting monitor windows as strings
-    //returns the IMW with lowest average
-    minAverage(){
-        let intervals = this._makeIntervals(this.vistorsPerMin)
-        let getWindows = this._getMonitorWindows(intervals)
-        // arr of tuples with all IMWs
-        let getIMWs = this._getIMW(getWindows)
-
-    // 	will be strings, make nums
-    	getIMWs = this._convertToInts(getIMWs)
-        console.log('wins', getIMWs)
-        let averages = getIMWs.map(arr => {
-            return arr.reduce((accumulator, currentValue) => {
-                return accumulator + currentValue
-            })
-        })
-        // divide the sum by number of items in window
-        .map((sum, index) => {
-            return(sum / getIMWs[index].length)
-        })//find lowest average in averages
-        let highAverage = averages.reduce((a, b) => {
-            return Math.min(a, b)
-        })
-        // find the index in the averages arr
-        let index = averages.indexOf(highAverage)
-        // index into the original arr to get solution
-        let highestWindow = getIMWs[index]
-        return highestWindow
-
-
-    }
+	// INSTANCE FUNC- call on instance to get IMW with the lowest average window
+	//takes array of interesting monitor windows as strings
+	//returns the IMW with lowest average
+	minAverage() {
+		let intervals = this._makeIntervals(this.vistorsPerMin)
+		let getWindows = this._getMonitorWindows(intervals)
+		// arr of tuples with all IMWs
+		let getIMWs = this._getIMW(getWindows)
+		// 	will be strings, make nums
+		getIMWs = this._convertToInts(getIMWs)
+		let averages = getIMWs.map(arr => {
+				return arr.reduce((accumulator, currentValue) => {
+					return accumulator + currentValue
+				})
+			})
+			// divide the sum by number of items in window
+			.map((sum, index) => {
+				return (sum / getIMWs[index].length)
+			}) //find lowest average in averages
+		let highAverage = averages.reduce((a, b) => {
+			return Math.min(a, b)
+		})
+		// find the index in the averages arr
+		let index = averages.indexOf(highAverage)
+		// index into the original arr to get solution
+		let highestWindow = getIMWs[index]
+		return highestWindow
+	}
 }
 
-// const test1 = new MonitoringWindow([1, 5, null, 1, 2, 2, null, 3])
-// // console.log(test1.maxSum()[0] === 1)
-// // console.log(test1.maxSum()[1] === 5)
-// //
-// //
-// const test2 = new MonitoringWindow([1, 5, null, 1, 2, 2, null, 3])
-// // console.log(test2.minAverage()[0] === 1)
-// // console.log(test2.minAverage()[1] === 2)
-// // console.log(test2.minAverage()[2] === 2)
+// UNIT TESTS
+const test1 = new MonitoringWindow([1, 5, null, 1, 2, 2, null, 3])
+console.log(test1.maxSum()[0] === 1)
+console.log(test1.maxSum()[1] === 5)
+//
+//
+const test2 = new MonitoringWindow([1, 5, null, 1, 2, 2, null, 3])
+console.log(test2.minAverage()[0] === 1)
+console.log(test2.minAverage()[1] === 2)
+console.log(test2.minAverage()[2] === 2)
 
-const test3 = new MonitoringWindow([1,90,null,4,22,66,12,89, null, undefined, NaN, 3000])
+const test3 = new MonitoringWindow([1, 90, null, 4, 22, 66, 12, 89, null, undefined, NaN, 3000])
 console.log(test3.maxSum()[0] === 3000)
-// let res = test3._getIMW([ [ 1 ],
-//   [ 1, 2 ],
-//   [ 2 ],
-//   [ 4 ],
-//   [ 4, 22 ],
-//   [ 4, 22, 66 ],
-//   [ 4, 22, 66, 12 ],
-//   [ 4, 22, 66, 12, 89 ],
-//   [ 22 ],
-//   [ 22, 66 ],
-//   [ 22, 66, 12 ],
-//   [ 22, 66, 12, 89 ],
-//   [ 66 ],
-//   [ 66, 12 ],
-//   [ 66, 12, 89 ],
-//   [ 12 ],
-//   [ 12, 89 ],
-//   [ 89 ] ])
-  // console.log(res)
-  // console.log(test3._convertToInts(test3._getIMW([ [ 1 ],
-  //   [ 1, 2 ],
-  //   [ 2 ],
-  //   [ 4 ],
-  //   [ 4, 22 ],
-  //   [ 4, 22, 66 ],
-  //   [ 4, 22, 66, 12 ],
-  //   [ 4, 22, 66, 12, 89 ],
-  //   [ 22 ],
-  //   [ 22, 66 ],
-  //   [ 22, 66, 12 ],
-  //   [ 22, 66, 12, 89 ],
-  //   [ 66 ],
-  //   [ 66, 12 ],
-  //   [ 66, 12, 89 ],
-  //   [ 12 ],
-  //   [ 12, 89 ],
-  //   [ 89 ] ]))))
-// console.log(test3._convertToInts(["1,2", "4,22,66,12,89", "13", "1010,1231,2,1231"]))
+console.log(test3.minAverage()[0] === 4)
--- email/sum_two_nums.js
+++ email/sum_two_nums.js
@@ -1,57 +1,56 @@
 // checks if any two arr values add up to a number
 // takes an array and a num to match
 // returns an object with sum: true/ false, and info of the two arr values
-function sumTwoNums(arr, num){
-    // set to true once a match is made
-    let sumMade = false
-    // outer loop controls num
-    for (var j = 0; j < arr.length; j++) {
-        let outerVal = arr[j]
-        // get index of outer- same as j
-        let outerValIndex = j
-        // inner loops over arr once for every num
-        for (var i = 0; i < arr.length; i++) {
-            let innerVal = arr[i]
-            // don't check outer against itself - if outer index matches the current index, skip
-             if(outerValIndex !== i){
-                let sum = outerVal + innerVal
-                //if sum, then return obj & true
-                if(sum === num){
-                    sumMade = true
-                    return {
-                        sum: true,
-                        numbers: [
-                            {
-                                first: outerVal,
-                                index: outerValIndex
-                            },
-                            {
-                                second: innerVal,
-                                index: i
-                            }
-                        ]
-                    }
-                }
-            }
-        }
-    }
-    // if not match, return false
-    if(!sumMade){
-        return false
-    }
+function sumTwoNums(arr, num) {
+	// set to true once a match is made
+	let sumMade = false
+	// outer loop controls num
+	for(var j = 0; j < arr.length; j++) {
+		let outerVal = arr[j]
+		// get index of outer- same as j
+		let outerValIndex = j
+		// inner loops over arr once for every num
+		for(var i = 0; i < arr.length; i++) {
+			let innerVal = arr[i]
+			// don't check outer against itself - if outer index matches the current index, skip
+			if(outerValIndex !== i) {
+				let sum = outerVal + innerVal
+				//if sum, then return obj & true
+				if(sum === num) {
+					sumMade = true
+					return {
+						sum: true,
+						numbers: [{
+								first: outerVal,
+								index: outerValIndex
+							},
+							{
+								second: innerVal,
+								index: i
+							}
+						]
+					}
+				}
+			}
+		}
+	}
+	// if not match, return false
+	if(!sumMade) {
+		return false
+	}
 }
 
 
 // UNIT TESTs
-const unitTest1 = sumTwoNums([22,3,14,5,-44, null], 17)
+const unitTest1 = sumTwoNums([22, 3, 14, 5, -44, null], 17)
 console.log(unitTest1.sum === true)
 console.log(unitTest1.numbers[0].first === 3)
 console.log(unitTest1.numbers[1].second === 14)
 
-const unitTest2 = sumTwoNums([55, 2,11,603,400, 29,66,32, 77, 791, 10,44, 4, 2, 99, 548, 790, 91, 839, 3429, 43, 129], 632)
+const unitTest2 = sumTwoNums([55, 2, 11, 603, 400, 29, 66, 32, 77, 791, 10, 44, 4, 2, 99, 548, 790, 91, 839, 3429, 43, 129], 632)
 console.log(unitTest2.sum === true)
 console.log(unitTest2.numbers[0].first === 603)
 console.log(unitTest2.numbers[1].second === 29)
 
-const unitTest3 = sumTwoNums([55, 2,11,603,400, 29,66,32, 77, 791, 10,44, 4, 2, 99, 548, 790, 91, 839, 3429, 43, 129], 433)
+const unitTest3 = sumTwoNums([55, 2, 11, 603, 400, 29, 66, 32, 77, 791, 10, 44, 4, 2, 99, 548, 790, 91, 839, 3429, 43, 129], 433)
 console.log(unitTest3 === false)
