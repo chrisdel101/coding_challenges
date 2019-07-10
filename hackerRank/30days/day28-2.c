157ecd61611b4d629f7424a249b6e2edbc76b3b5
Chris Del <drumgod101@gmail.com>
2019-07-10 19:55:42 -0400

    getting closer to completing

--- hackerRank/30days/day28-2.c
+++ hackerRank/30days/day28-2.c
@@ -5,6 +5,7 @@
 #include <stdbool.h>
 #define NAME_LENGTH 45
 #define HASHTABLE_SIZE 65536
+
 typedef struct Node
 {
     char name[NAME_LENGTH];
@@ -12,78 +13,84 @@ typedef struct Node
 
 } Node;
 
-Node *insertBefore(Node *list, char *inputName, Node *newNode)
-{
-    Node *tempList = list;
-    // if 2 or more nodes go here
-    // printf("Input name %s\n", inputName);
-    // printf("check name %s\n", tempList->name);
-    while (tempList->next != NULL)
-    {
-        // // check if matches current
-        if (strcmp(tempList->name, inputName) > 0)
-        {
-            Node *tempNode = tempList;
-            tempList = newNode;
-            newNode->next = tempNode;
-            // printf("end name %s\n", tempList->name);
-            return tempList;
-        }
-        // {
-        // }
-        // else if (strcmp(tempList->next->name, insertBeforeName) == 0)
-        // {
-        //     Node *tempNext = tempList->next;
-        //     tempList->next = newNode;
-        //     newNode->next = tempNext;
-        //     return tempList;
-        // }
-        tempList = tempList->next;
-        // printf("looping %s\n", tempList->name);
-        // if(temp)
-    }
-    // if there is only one node - check only first node
-    if (strcmp(tempList->name, inputName) > 0)
-    {
-        Node *tempNode = tempList;
-        tempList = newNode;
-        newNode->next = tempNode;
-        // printf("current %s\n", tempList->name);
-        // printf("next %s\n", tempList->next->name);
-        // printf("end name %s\n", tempList->name);
-        return tempList;
-    }
-    // if (strcmp(tempList->name, insertBeforeName) == 0)
-    // {
-    //     // puts("one node");
-    //     // printf("inputName %s\n", inputName);
-    //     Node *tempNext = tempList; //null
-    //     tempList = newNode;
-    //     newNode->next = tempNext;
-    //     return tempList;
-    // }
+void display(Node *head);
+// Node *insertBefore(Node *list, char *inputName, Node *newNode)
+// {
+//     puts("before");
+//     Node *tempList = list;
+//     // if 2 or more nodes go here
+//     printf("Input name %s\n", inputName);
+//     printf("check name %s\n", tempList->name);
+//     while (tempList->next != NULL)
+//     {
+//         printf("looping %s\n", tempList->name);
+//         // // check if matches current
+//         if (strcmp(tempList->name, inputName) > 0 || strcmp(tempList->name, inputName) == 0)
+//         {
+//             Node *tempNode = tempList;
+//             tempList = newNode;
+//             newNode->next = tempNode;
+//             // printf("end name %s\n", tempList->name);
+//             return tempList;
+//         }
+//         // {
+//         // }
+//         // else if (strcmp(tempList->next->name, insertBeforeName) == 0)
+//         // {
+//         //     Node *tempNext = tempList->next;
+//         //     tempList->next = newNode;
+//         //     newNode->next = tempNext;
+//         //     return tempList;
+//         // }
+//         tempList = tempList->next;
+//         // printf("looping %s\n", tempList->name);
+//         // if(temp)
+//     }
+//     // if there is only one node - check only first node
+//     if (strcmp(tempList->name, inputName) > 0)
+//     {
+//         Node *tempNode = tempList;
+//         tempList = newNode;
+//         newNode->next = tempNode;
+//         // printf("current %s\n", tempList->name);
+//         // printf("next %s\n", tempList->next->name);
+//         // printf("end name %s\n", tempList->name);
+//         return tempList;
+//     }
+//     // if (strcmp(tempList->name, insertBeforeName) == 0)
+//     // {
+//     //     // puts("one node");
+//     //     // printf("inputName %s\n", inputName);
+//     //     Node *tempNext = tempList; //null
+//     //     tempList = newNode;
+//     //     newNode->next = tempNext;
+//     //     return tempList;
+//     // }
 
-    // printf("END END name %s\n", tempList->name);
-    // puts("No change");
-    return list;
-    // }
-}
-Node *insertAfter(Node *head, char *name, Node *newNode)
-{
-    Node *tempList = head;
-    while (tempList->next != NULL)
-    {
-        // puts("INSIDE\n");
+//     // printf("END END name %s\n", tempList->name);
+//     // puts("No change");
+//     return list;
+//     // }
+// }
+// Node *insertAfter(Node *head, char *name, Node *newNode)
+// {
+//     puts("after");
+//     Node *tempList = head;
+//     while (tempList->next != NULL)
+//     {
 
-        tempList = tempList->next;
-        // printf("current %s\n", tempList->name);
-    }
-    tempList->next = newNode;
-    return head;
-}
+//         // printf("current %s\n", tempList->name);
+//         tempList = tempList->next;
+//     }
+//     printf("last %s\n", tempList->name);
+//     tempList->next = newNode;
+//     return head;
+// }
 Node *transverseAdd(Node *head, char *name)
 {
 
+    Node *displayList = head;
+    display(displayList);
     Node *tempList = head;
     Node *newNode = malloc(sizeof(Node));
     int length = 0;
@@ -97,20 +104,73 @@ Node *transverseAdd(Node *head, char *name)
     if (tempList == NULL)
     {
         tempList = newNode;
+        // printf("end name %s\n", tempList->name);
         return tempList;
     }
     else
     {
+        for (Node *node = tempList; node; node = node->next)
+        {
 
-        tempList = insertBefore(head, name, newNode);
+            printf("looping %s\n", node->name);
+            if (node->next && (strcmp(node->next->name, name) > 0 || strcmp(node->next->name, name) == 0))
+            {
+                printf("name %s\n", node->name);
+                printf("input name %s\n", name);
+                puts("\n");
+                Node *tempNext = node->next;
+                newNode->next = tempNext;
+                node->next = newNode;
+                newNode->next = tempNext;
+                return node;
+            }
+            // check if current is greater than name -> put before current
+            else if (strcmp(node->name, name) > 0 || strcmp(node->name, name) == 0)
+            {
+                printf("name %s\n", node->name);
+                printf("input name %s\n", name);
+                puts("\n");
+                Node *tempNode = node;
+                node = newNode;
+                newNode->next = tempNode;
+                return node;
+            }
+            // node = node->next;
+            // printf("end name %s\n", tempList->name);
+        }
+        // while (tempList->next != NULL)
+        // {
+        //     // // check if matches current
+        //     if (strcmp(tempList->name, name) > 0 || strcmp(tempList->name, name) == 0)
+        //     {
+        //         puts("HERE 1");
+        //         Node *tempNode = tempList;
+        //         tempList = newNode;
+        //         newNode->next = tempNode;
+        //         return tempList;
+        //     }
+        //     tempList = tempList->next;
+        //     // printf("end name %s\n", tempList->name);
+        // }
+        // if (strcmp(tempList->name, name) > 0)
+        // {
+        //     Node *tempNode = tempList;
+        //     tempList = newNode;
+        //     newNode->next = tempNode;
+        //     // printf("end name %s\n", tempList->name);
+        //     return tempList;
+        // }
     }
-    // printf("return name %s\n", tempList->name);
-    if (tempList->name == head->name)
+    tempList = head;
+    for (Node *node = tempList; node; node = node->next)
     {
-        // puts("HERE");
-        tempList = insertAfter(head, name, newNode);
+
+        printf("current %s\n", node->name);
+        tempList = node;
     }
-    return tempList;
+    printf("last %s\n", tempList->name);
+    tempList->next = newNode;
+    return head;
 }
 bool emailChecker(char *name, char *email)
 {
@@ -126,9 +186,10 @@ void display(Node *head)
     Node *start = head;
     while (start)
     {
-        printf("name %s\n ", start->name);
+        printf("output %s\n", start->name);
         start = start->next;
     }
+    printf("\n");
 }
 int main(void)
 {
@@ -142,10 +203,11 @@ int main(void)
         memset(name, 0, sizeof(name));
         memset(email, 0, sizeof(email));
         scanf("%s %s", name, email);
-        if (emailChecker(name, email))
-        {
-            list = transverseAdd(list, name);
-        }
+        // if (emailChecker(name, email))
+        // {
+        list = transverseAdd(list, name);
+        // display(list);
+        // }
         // display list
         // printf("head %s\n", list->name);
         // printf("head %i\n", head->next);
